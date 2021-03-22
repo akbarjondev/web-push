224daeebaea3a82f9d3ad25d1d608f7ee116d7a7
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-03-22 16:27:20 +0500

    Add subscribe

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
+const cors = require('cors')
+const express = require('express')
 const pushManager = require('web-push')
 
-// console.log(pushManager.generateVAPIDKeys())
+const port = process.env.PORT || 3001
+const app = express()
 
-const publicKey = 'BBrSLCPyECboR8i2adR0VZ0qbIpY96Is0NSgSfRxZ4eYscNubEAbIVl14RbR68DTpqzznRETBDlPamZZzZ9s4Tw'
-const privateKey = 'BSXTTC0NCKpaqPM_Toiyhjd7bZh35n0xp-6uhguOruE'
+app.use(express.json())
+app.use(cors())
+
+const subscribers = []
+
+const { publicKey, privateKey } = pushManager.generateVAPIDKeys()
 
 pushManager.setVapidDetails(
 	"mailto:gimmick.data@gmail.com",
@@ -11,12 +18,38 @@ pushManager.setVapidDetails(
 	privateKey
 )
 
-const subscriber = {
-  "endpoint": "https://updates.push.services.mozilla.com/wpush/v2/gAAAAABgWCp-rn5VWbEugEe1VITP3tila_rC4ORL7FpMPIXNLCmLgTEac6_pxQ53zmtKfxQFQFMBtN80t7cqucLliA0Gh8Eoj0rMUDqbWeL-T190jvvj9j7n_qwFMOo39I4yiUvKlk128eXS9e7UqTRZxEwOg74blPvNY8-0r0Sutu3-F3OYmrM",
-  "keys": {
-    "auth": "HL6vnJ41ZN_mZ1kJ_4S42g",
-    "p256dh": "BLAcvh23nKQqqHmu3ZEdWmCB4dgKufE0vsduAzJROT8UD50MM-sX7hS2zbYzAsE7C2Aopfuir1t70TFRoN8oHGg"
-  }
-}
+app.get('/publickey', (req, res) => {
 
-pushManager.sendNotification(subscriber, 'Assalomu alaykum!')
+	try {
+		
+		res.send({
+			publicKey
+		}).end()
+
+	} catch(e) {
+		console.log(e)
+	}
+
+})
+
+app.post('/subscribe', (req, res) => {
+
+	try {
+
+		const { subscriber } = req.body
+
+		subscribers.push(subscriber)
+
+		pushManager.sendNotification(subscriber, 'Obuna yoqildi!')
+		
+		res.send({
+			status: 200
+		}).end()
+
+	} catch(e) {
+		console.log(e)
+	}
+
+})
+
+app.listen(port, () => console.log(`ready at http://localhost:${port}`))
